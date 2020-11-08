a11d47887f69ea28049a6acc08265f7e4e7fdb83
Sahanon Phisetpakasit <sahanon.p@ku.th>
2020-11-08 20:00:29 +0700

    add model for admin

--- noxusProject/models.py
+++ noxusProject/models.py
@@ -8,7 +8,6 @@ class ImageSpell(models.Model):
     e = models.CharField(max_length = 200,default=None)
     r = models.CharField(max_length = 200,default=None)
 
-
 class Spell(models.Model):
     passive = models.CharField(max_length=200,default=None)
     q = models.CharField(max_length=200,default=None)
@@ -19,18 +18,34 @@ class Spell(models.Model):
 
 
 class Items(models.Model):
+    name = models.TextField(max_length= 200,default=None)
     description = models.TextField(max_length=200,default=None)
     img = models.CharField(max_length=200,default=None)
-
-
-class Rune(models.Model):
-    img = models.CharField(max_length=200,default=None)
+    def __str__(self):
+        return self.name
 
 class ImageChampion(models.Model):
     main = models.CharField(max_length=200, default= None, null= True)
     splash_art = models.CharField(max_length=200, default= None, null= True)
+
+class ItemChampion(models.Model):
+    name = models.TextField(max_length=200)
+    starter1 = models.IntegerField(default=0)
+    starter2 = models.IntegerField(default=0)
+    items_1 = models.IntegerField(default=0)
+    items_2 = models.IntegerField(default=0)
+    items_3 = models.IntegerField(default=0)
+    items_4 = models.IntegerField(default=0)
+    items_5 = models.IntegerField(default=0)
+    items_6 = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} Build"
+
 class Champion(models.Model):
     name = models.CharField(max_length = 200, null=True)
     title = models.CharField(max_length=200, default= None, null= True)
     img = models.ForeignKey(ImageChampion,on_delete= models.CASCADE, default=None, null=True)
     spell = models.ForeignKey(Spell, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
