46f1031a26c49f4135979646df25acba0d02e131
Potchara J <59822724+VonKiar@users.noreply.github.com>
2020-11-16 01:03:31 +0700

    initialize new feature branch

--- noxusProject/urls.py
+++ noxusProject/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:role>',views.index, name = "index/role"),
+    path('<str:role>', views.index, name = "index/role"),
     path("champion/<str:champion_name>" , views.detail, name = 'detail')
 ]
