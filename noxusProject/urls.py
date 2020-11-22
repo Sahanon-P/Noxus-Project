bb68367a98504173310e4f631a02b6a4f163f873
Sahanon Phisetpakasit <sahanon.p@ku.th>
2020-11-22 23:27:45 +0700

    make authentication

--- noxusProject/urls.py
+++ noxusProject/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index,name="index"),
     path('<str:role>',views.index, name = "index-role"),
     path("champion/<str:champion_name>" , views.detail, name = 'detail'),
-    path("contact/",views.contact, name= "contact")
+    path("contact/",views.contact, name= "contact"),
+    path("user/register/",views.register, name = 'register'),
+    path("user/login/",views.loginPage, name = 'login'),
+    path("user/logout/",views.logoutPage, name = 'logout'),
+
 ]
