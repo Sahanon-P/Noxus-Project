b0cd4eebb9f36bd8b6d1016842a880e76305a306
Thornthep Chomchuen <thornthep.c@ku.th>
2020-11-23 00:42:13 +0700

    urgent bug fix

--- noxusProject/urls.py
+++ noxusProject/urls.py
@@ -2,7 +2,7 @@ from noxusProject.views import search
 from django.urls import path
 from . import views
 
-app_name = "noxusProject" 
+# app_name = "noxusProject" 
 urlpatterns = [
     path('', views.index,name="index"),
     path('<str:role>',views.index, name = "index-role"),
