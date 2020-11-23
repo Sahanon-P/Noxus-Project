ba9df643472b2a8f35bc65eb26c8893375bf842c
Thornthep Chomchuen <thornthep.c@ku.th>
2020-11-23 23:06:06 +0700

    hotfix searchbar conflict

--- noxusProject/models.py
+++ noxusProject/models.py
@@ -91,6 +91,8 @@ class Champion(models.Model):
     support = models.BooleanField(default=False)
     def __str__(self):
         return self.name
+    def lower(self):
+        return self.lower()
 
 class SummonnerSpell(models.Model):
     name = models.CharField(max_length = 200, null=True)
