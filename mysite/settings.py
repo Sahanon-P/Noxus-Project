51795d2a4d6f03d778e20630dcdc51e7fd2a673d
Sahanon Phisetpakasit <sahanon.p@ku.th>
2020-12-14 11:52:45 +0700

    fix security issue and add delete and update in views

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,10 +18,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&qx220rcfja!9dcr)@g%$erw20rj7xte8xqeymqou2^5@bdac^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = ['noxus-project.herokuapp.com']
 
